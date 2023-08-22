6677e64e52a9700861ed8aa33882cc94537d9808
Svetlana Nikulina <svetlana.nikulina@devexpress.com>
2023-08-22 13:42:47 +0400

    Update Default.aspx.cs

--- CS/Default.aspx.cs
+++ CS/Default.aspx.cs
@@ -6,16 +6,12 @@ using System.Linq;
 using DevExpress.Web.Data;
 using DevExpress.Web;
 
-public partial class _Default : System.Web.UI.Page
-{
-    protected List<GridDataItem> GridData
-    {
-        get
-        {
+public partial class _Default : System.Web.UI.Page {
+    protected List<GridDataItem> GridData {
+        get {
             var key = "34FAA431-CF79-4869-9488-93F6AAE81263";
             if (!IsPostBack || Session[key] == null)
-                Session[key] = Enumerable.Range(0, 10).Select(i => new GridDataItem
-                {
+                Session[key] = Enumerable.Range(0, 10).Select(i => new GridDataItem {
                     ID = i,
                     C1 = "This is a simple text " + i.ToString(),
                     C2 = "This is a simple text " + i.ToString(),
@@ -27,21 +23,16 @@ public partial class _Default : System.Web.UI.Page
             return (List<GridDataItem>)Session[key];
         }
     }
-    protected void Page_Load(object sender, EventArgs e)
-    {
+    protected void Page_Load(object sender, EventArgs e) {
         Grid.DataSource = GridData;
         Grid.DataBind();
     }
-
-    protected void Grid_HtmlDataCellPrepared(object sender, ASPxGridViewTableDataCellEventArgs e)
-    {
+    protected void Grid_HtmlDataCellPrepared(object sender, ASPxGridViewTableDataCellEventArgs e) {
         if (e.DataColumn.FieldName == "C6")
             if (e.CellValue != null)
                 e.Cell.ToolTip = e.CellValue.ToString();
     }
-
-    public class GridDataItem
-    {
+    public class GridDataItem {
         public int ID { get; set; }
         public string C1 { get; set; }
         public string C2 { get; set; }
